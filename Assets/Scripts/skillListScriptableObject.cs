58b07b7488141a7e866052b09ca2ab4c10f8c9f3
DimChristodoulou <dchristodoulou.di@gmail.com>
2019-07-30 22:20:45 +0300

    added skill list scriptable object

---
+++ Assets/Scripts/skillListScriptableObject.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName="New Skill List", menuName = "Skill List")]
+public class skillListScriptableObject : ScriptableObject
+{
+    public List<skills> skills;
+}
