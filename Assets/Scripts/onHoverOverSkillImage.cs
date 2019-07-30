becc1aac6d63723ed78715e1d2dcbbf9176fdc14
DimChristodoulou <dchristodoulou.di@gmail.com>
2019-07-30 22:31:57 +0300

    skillsSO to ui panel

--- Assets/Scripts/onHoverOverSkillImage.cs
+++ Assets/Scripts/onHoverOverSkillImage.cs
@@ -7,7 +7,7 @@ using UnityEngine.EventSystems;
 public class onHoverOverSkillImage : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     public skillListScriptableObject allSkills;
-
+    private String[] temp;
     private void Start(){
         allSkills = Resources.Load("allSkills") as skillListScriptableObject;
         allSkills.skills.Sort((p1,p2)=>p1.id.CompareTo(p2.id));
@@ -17,13 +17,16 @@ public class onHoverOverSkillImage : MonoBehaviour, IPointerEnterHandler, IPoint
     public void OnPointerEnter(PointerEventData pointerEventData)
     {
         //Output to console the GameObject's name and the following message
-        Debug.Log("Cursor Entering " + name + " GameObject");
+        //Debug.Log("Cursor Entering " + name + " GameObject");
+        temp = name.Split('_');
+        Debug.Log(allSkills.skills[int.Parse(temp[temp.Length - 1])].skill_name);
     }
 
     //Detect when Cursor leaves the GameObject
     public void OnPointerExit(PointerEventData pointerEventData)
     {
         //Output the following message with the GameObject's name
-        Debug.Log("Cursor Exiting " + name + " GameObject");
+        //Debug.Log("Cursor Exiting " + name + " GameObject");
+        Debug.Log(allSkills.skills[int.Parse(temp[temp.Length - 1])].skill_name);
     }
 }
