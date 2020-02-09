97247bae3ff873559ee16cba79ec1f2acb4ea00c
DimChristodoulou <dchristodoulou.di@gmail.com>
2020-02-09 20:31:38 +0200

    Procedural dungeon generation - 1 - Refactoring and random floors

--- Assets/Scripts/LevelGeneration/levelGeneration.cs
+++ Assets/Scripts/LevelGeneration/levelGeneration.cs
@@ -4,11 +4,15 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using Random = UnityEngine.Random;
 
 public class levelGeneration : MonoBehaviour
 {
 
-    [SerializeField] private List<GameObject> _levelTiles;
+    //[SerializeField] private List<GameObject> _levelTiles;
+    [SerializeField] private List<GameObject> _floorTiles;
+    [SerializeField] private List<GameObject> _wallTiles;
+    [SerializeField] private List<GameObject> _environmentals;
     [SerializeField] private TMP_InputField xDim;
     [SerializeField] private TMP_InputField yDim;
     [SerializeField] private Transform parentFolder;
@@ -45,7 +49,7 @@ public class levelGeneration : MonoBehaviour
         List<float> widths = new List<float>();
         List<float> lengths = new List<float>();
         
-        foreach (GameObject tile in _levelTiles){
+        foreach (GameObject tile in _floorTiles){
             widths.Add(tile.GetComponent<MeshRenderer>().bounds.size.z);
             lengths.Add(tile.GetComponent<MeshRenderer>().bounds.size.x);
         }
@@ -60,33 +64,33 @@ public class levelGeneration : MonoBehaviour
         for (int i = 0; i < x; i++){
             for (int j = 0; j < y; j++) {
                 //Create an x BY y array of tiles and place them in the correct distance
-                tiles[i,j] = Instantiate(_levelTiles[0]);
+                tiles[i,j] = Instantiate(_floorTiles[Random.Range(0, _floorTiles.Count)]);
                 tiles[i,j].transform.position = new Vector3(i*widths[0], 0, j*widths[0]);
                 tiles[i,j].transform.SetParent(parentFolder);
                 tiles[i, j].name = "floor_" + i + "_" + j;
                 
                 //Create horizontal & vertical walls
                 if (i == NORTH_WALL) {
-                    tiles[i,j] = Instantiate(_levelTiles[1]);
+                    tiles[i,j] = Instantiate(_wallTiles[0]);
                     tiles[i,j].transform.position = new Vector3(i - widths[0]/2, 0, j*widths[0]);
                     tiles[i,j].transform.SetParent(parentFolder);
                     tiles[i, j].name = "north_wall_" + i + "_" + j;
                 }
                 else if (i == SOUTH_WALL) {
-                    tiles[i,j] = Instantiate(_levelTiles[1]);
+                    tiles[i,j] = Instantiate(_wallTiles[0]);
                     tiles[i,j].transform.position = new Vector3(i*widths[0] + widths[0]/2, 0, j*widths[0]);
                     tiles[i,j].transform.SetParent(parentFolder);
                     tiles[i, j].name = "south_wall_" + i + "_" + j;
                 }
                 else if (j == EAST_WALL) {
-                    tiles[i,j] = Instantiate(_levelTiles[1]);
+                    tiles[i,j] = Instantiate(_wallTiles[0]);
                     tiles[i,j].transform.position = new Vector3(i*widths[0], 0, j*1.15f - widths[0]/2);
                     tiles[i,j].transform.Rotate(0,90,0);
                     tiles[i, j].name = "east_wall_" + i + "_" + j;
                     tiles[i,j].transform.SetParent(parentFolder);
                 }
                 else if (j == WEST_WALL) {
-                    tiles[i,j] = Instantiate(_levelTiles[1]);
+                    tiles[i,j] = Instantiate(_wallTiles[0]);
                     tiles[i,j].transform.position = new Vector3(i*widths[0], 0, j*widths[0] + widths[0]/2);
                     tiles[i,j].transform.Rotate(0,90,0);
                     tiles[i, j].name = "west_wall_" + i + "_" + j;
@@ -95,28 +99,28 @@ public class levelGeneration : MonoBehaviour
                 
                 //Create corners
                 if (i == NORTH_WALL && j == EAST_WALL) {
-                    tiles[i,j] = Instantiate(_levelTiles[1]);
+                    tiles[i,j] = Instantiate(_wallTiles[0]);
                     tiles[i,j].transform.position = new Vector3(i*widths[0], 0, j*1.15f - widths[0]/2);
                     tiles[i,j].transform.Rotate(0,90,0);
                     tiles[i,j].transform.SetParent(parentFolder);
                     tiles[i, j].name = "NE_wall_" + i + "_" + j;
                 }
                 else if (i == NORTH_WALL && j == WEST_WALL) {
-                    tiles[i,j] = Instantiate(_levelTiles[1]);
+                    tiles[i,j] = Instantiate(_wallTiles[0]);
                     tiles[i,j].transform.position = new Vector3(i*widths[0], 0, j*widths[0] + widths[0]/2);
                     tiles[i,j].transform.Rotate(0,90,0);
                     tiles[i, j].name = "NW_wall_" + i + "_" + j;
                     tiles[i,j].transform.SetParent(parentFolder);
                 }
                 else if (i == SOUTH_WALL && j == EAST_WALL) {
-                    tiles[i,j] = Instantiate(_levelTiles[1]);
+                    tiles[i,j] = Instantiate(_wallTiles[0]);
                     tiles[i,j].transform.position = new Vector3(i*widths[0], 0, j*1.15f - widths[0]/2);
                     tiles[i,j].transform.Rotate(0,90,0);
                     tiles[i, j].name = "SE_wall_" + i + "_" + j;
                     tiles[i,j].transform.SetParent(parentFolder);
                 }
                 else if (i == SOUTH_WALL && j == WEST_WALL) {
-                    tiles[i,j] = Instantiate(_levelTiles[1]);
+                    tiles[i,j] = Instantiate(_wallTiles[0]);
                     tiles[i,j].transform.position = new Vector3(i*widths[0], 0, j*widths[0] + widths[0]/2);
                     tiles[i,j].transform.Rotate(0,90,0);
                     tiles[i, j].name = "SW_wall_" + i + "_" + j;
